8b226cb30897b829103ebee2480734fcd0104eb3
401 <401@academicos.cesde.edu.co>
2022-11-18 12:21:21 -0500

    Se crea menú de opciones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner entradaDeDato = new Scanner(System.in);
+
+        int opcionMenu;
+
+        System.out.println("1. Registro empleado \n2. Registrar viaje \n3. Ver empleados \n4. Ver viajes de un empleado \n0. Salir");
+        opcionMenu = entradaDeDato.nextInt();
+
+        do {
+            switch (opcionMenu){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+
+            }
+
+        }while (opcionMenu!=0);
     }
 }
